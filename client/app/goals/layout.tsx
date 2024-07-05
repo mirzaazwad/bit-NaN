6e381593fd8baab4e0c063a493b4e0a60616e57e
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-07-05 14:36:59 +0600

    Goals start

---
+++ client/app/goals/layout.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/app/components/footer/Footer";
+import NavBar from "../components/navbar/NavBarLoggedIn";
+
+
+const GoalsLayout = ({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+    return ( 
+        <div className="flex flex-col min-h-screen w-full">
+            <NavBar/>
+            <main className="flex-grow min-h-screen">
+                {children}
+            </main>
+            <Footer/>
+        </div>
+
+     );
+}
+ 
+export default GoalsLayout;
