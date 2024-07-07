ccd95b878776f9581c55c1d0cfd2bbae75f8d77c
Mirza Mohammad Azwad <mirzaazwad8@gmail.com>
2024-07-07 19:19:29 +0600

    Fixing Documentation Error

---
+++ server/src/main/java/com/example/server/Goals/Core/DataTransferObjects/TaskRequest.java
@@ -0,0 +1,19 @@
+package com.example.server.Goals.Core.DataTransferObjects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskRequest {
+    private String Title;
+    private String Description;
+    private String Status;
+    private Date StartTime;
+    private Date EndTime;
+    private String Notes;
+}
