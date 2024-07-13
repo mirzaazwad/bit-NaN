2f05600ab44200f347cfc5b7d59f70b012c166be
Mirza Mohammad Azwad <mirzaazwad8@gmail.com>
2024-07-13 17:25:05 +0600

    RouteProtectionCorsIssue

---
+++ server/src/main/java/com/example/server/Authentication/Entity/TokenEntity.java
@@ -0,0 +1,4 @@
+package com.example.server.Authentication.Entity;
+
+public class TokenEntity {
+}
---
+++ server/src/main/java/com/example/server/Authentication/Repository/TokenRepository.java
@@ -0,0 +1,4 @@
+package com.example.server.Authentication.Repository;
+
+public class TokenRepository {
+}
