06927c880b191ab56a5d1df8e066140fd038122a
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2024-07-29 00:05:55 +0600

    File category and other changes

--- server/src/main/java/com/example/server/Profile/Controller/ProfileController.java
+++ server/src/main/java/com/example/server/Profile/Controller/ProfileController.java
@@ -4,7 +4,7 @@ import com.example.server.Profile.Core.DataTransferObjects.ProfileRequest;
 import com.example.server.Profile.Core.Interfaces.IProfileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +19,12 @@ public class ProfileController {
     private final IProfileService service;
 
     @PostMapping("/update")
-    public void update(@RequestBody ProfileRequest request){
+    public ResponseEntity<?> update(@RequestBody ProfileRequest request){
         try{
             service.update(request);
+            return ResponseEntity.ok("Profile updated successfully");
         }catch(Exception e){
-
+            return ResponseEntity.status(500).body("Error updating profile "+ e);
         }
     }
 }
