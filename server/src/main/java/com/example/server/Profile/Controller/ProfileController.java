86851561e8fdab2ce96ed57a4fd3e111b9d7ad9f
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2024-07-28 23:50:53 +0600

    minor change

--- server/src/main/java/com/example/server/Profile/Controller/ProfileController.java
+++ server/src/main/java/com/example/server/Profile/Controller/ProfileController.java
@@ -2,15 +2,18 @@ package com.example.server.Profile.Controller;
 
 import com.example.server.Profile.Core.DataTransferObjects.ProfileRequest;
 import com.example.server.Profile.Core.Interfaces.IProfileService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/profile")
+@Tag(name = "Profile")
 public class ProfileController {
 
     private final IProfileService service;
