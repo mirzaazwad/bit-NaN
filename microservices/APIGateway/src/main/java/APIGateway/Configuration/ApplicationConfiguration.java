4f43822d1e0734e6317a8e8db1be4552e96ff00d
azwadmirza <mirzaazwad8@gmail.com>
2024-08-09 10:45:17 +0600

    Solving CORS issue for microservices using Deduplication Filter corsConfigurationSource as per documentation

---
+++ microservices/APIGateway/src/main/java/APIGateway/Configuration/ApplicationConfiguration.java
@@ -0,0 +1,32 @@
+package APIGateway.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
--- microservices/APIGateway/src/main/java/APIGateway/Configuration/SecurityConfiguration.java
+++ microservices/APIGateway/src/main/java/APIGateway/Configuration/SecurityConfiguration.java
@@ -1,17 +1,24 @@
 package APIGateway.Configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @EnableWebFluxSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+
 
     private static final String[] WHITELIST_URL = {
             "/api/auth/**",
@@ -34,15 +41,19 @@ public class SecurityConfiguration {
 
 
 
+
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, AuthenticationConverter jwtAuthConverter, AuthenticationManager jwtAuthManager) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, AuthenticationConverter jwtAuthConverter, AuthenticationManager jwtAuthManager, CorsConfigurationSource corsConfigurationSource) {
         AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(jwtAuthManager);
         jwtFilter.setServerAuthenticationConverter(jwtAuthConverter);
+        CorsWebFilter corsFilter = new CorsWebFilter(corsConfigurationSource);
 
         return http
+                .addFilterBefore(corsFilter, SecurityWebFiltersOrder.CORS)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
                         .pathMatchers(WHITELIST_URL).permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHORIZATION)
@@ -51,4 +62,5 @@ public class SecurityConfiguration {
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
     }
+
 }
