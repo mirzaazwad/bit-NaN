47070d42134e108a35da7da3584aa1265019460d
mirzaazwad <mirzaazwad8@gmail.com>
2024-09-25 02:38:20 +0600

    Forum Card Handling

--- client/src/features/forum/components/Forum/CreateForumModal.tsx
+++ client/src/features/forum/components/Forum/CreateForumModal.tsx
@@ -1,9 +1,5 @@
-import { Button, Loader, Message, Modal, RadioTile, RadioTileGroup } from 'rsuite';
+import { Button, Loader, Message, Modal } from 'rsuite';
 import { InputComponent } from '../../../../components/goal/CommonComponents';
-import { IonIcon } from '@ionic/react';
-import { chatbubbleEllipsesSharp, documentSharp } from 'ionicons/icons';
-import FileInput from './FileInput';
-import { ForumType } from '../../../../utils/templates/Forum';
 import { useCreateForum } from '../../hooks/useCreateForum';
 
 interface ICreateForumModal {
@@ -15,14 +11,9 @@ interface ICreateForumModal {
 const CreateForumModal = ({ open, setOpen }: ICreateForumModal) => {
 
     const {
-        addFile,
         handleSubmit,
         handleInputChange,
         removeModal,
-        attachedFile,
-        forumType,
-        setForumType,
-        fileError,
         forumData,
         disabled,
         forumError
@@ -59,32 +50,6 @@ const CreateForumModal = ({ open, setOpen }: ICreateForumModal) => {
                         <InputComponent placeholder='Forum Title' value={forumData.title} onChange={value=>handleInputChange('title',value)}/>
                         <h5 className="text-lg font-semibold text-gray-800">Description</h5>
                         <InputComponent placeholder='Describe the contents of the Forum' value={forumData.description} onChange={value=>handleInputChange('description',value)}/>
-                        <h5 className="text-lg font-semibold text-gray-800">Type</h5>
-                        <p className="text-sm text-gray-400">Select the type of forum you want</p>
-                        <RadioTileGroup defaultValue="CHAT" aria-label="Visibility Level">
-                            <RadioTile icon={<IonIcon icon={chatbubbleEllipsesSharp} />} label="Chat" value="CHAT" onClick={() => setForumType(ForumType.CHAT)}>
-                                Chat with the AI model on different topics that suit your interest and academic demands
-                            </RadioTile>
-                            <RadioTile icon={<IonIcon icon={documentSharp} />} label="Document" value="DOCUMENT" onClick={() => setForumType(ForumType.DOCUMENT)}>
-                                Chat with the AI based on a particular PDF document such that the AI model analyzes the given document and answers as per the document's contents
-                            </RadioTile>
-                        </RadioTileGroup>
-                        {forumType === ForumType.DOCUMENT && (
-                            <>
-                                <h5 className="text-lg font-semibold text-gray-800">Insert A Document</h5>
-                                <FileInput setFile={addFile} />
-                                {attachedFile && (
-                                    <Message type='warning'>
-                                        <span className='font-semibold'>{attachedFile}</span>
-                                    </Message>
-                                )}
-                                {fileError && (
-                                    <Message type='error'>
-                                        <span className='font-semibold'>{fileError}</span>
-                                    </Message>
-                                )}
-                            </>
-                        )}
                     </div>
                 </Modal.Body>
                 <Modal.Footer className='p-2'>
